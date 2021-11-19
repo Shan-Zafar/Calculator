7a1529b1ad9ce8dc4405c7e61bf7a9da787d3071
Shan Zafar <bitf18545@pucit.edu.pk>
2021-11-19 18:10:17 +0500

    Another Commit

--- app/src/main/java/com/shanzafar/calculator/MainActivity.java
+++ app/src/main/java/com/shanzafar/calculator/MainActivity.java
@@ -1,14 +1,36 @@
 package com.shanzafar.calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextSwitcher;
+import android.widget.TextView;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    EditText et1, et2;
+    Button add, sub, mul, div;
+    TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        et1 = findViewById(R.id.et1);
+        et2 = findViewById(R.id.et2);
+        add = findViewById(R.id.add);
+        sub = findViewById(R.id.sub);
+        mul = findViewById(R.id.mul);
+        div = findViewById(R.id.div);
+        tv = findViewById(R.id.tv);
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                int f, s, a;
+            }
+        });
+
     }
 }
